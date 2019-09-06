b8ee6a33eb831d5a243446f61030c1a469747ec9
John Azzaro <jazzaro@temple.edu>
2019-09-05 22:07:17 -0700

    duration property

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 function splitScroll() {
     const controller = new ScrollMagic.Controller();                 // Controller that handles all the scrolls on the page.
+
+    new ScrollMagic.Scene({                                          // create a new Scrollmagic scene with an object with the following properties:
+        duration: 500,                                               // scroll duration that lasts 500 miliseconds.
+    });
 }
 
 
