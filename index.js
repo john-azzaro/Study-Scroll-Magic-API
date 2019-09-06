08ce7f5038a432d24303adbf6444b8ae4ab3815d
John Azzaro <jazzaro@temple.edu>
2019-09-05 22:25:15 -0700

    triggers, controllers

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ function splitScroll() {
     const controller = new ScrollMagic.Controller();                 // Controller that handles all the scrolls on the page.
 
     new ScrollMagic.Scene({                                          // create a new Scrollmagic scene with an object with the following properties:
-        duration: 500,                                               // scroll duration that lasts 500 miliseconds.
-    });
+        duration: 500,                                               // scroll duration that lasts 500 pixels.
+        triggerElement: '.about-title',                              // Trigger element that defines what will trigger the scroll (in this case, it is "about-title") so when it reaches this element, scroll effect will execute
+    })
+    .addIndicators()                                                  // method to see the triggers during development
+    .addTo(controller);
+    
 }
 
 
