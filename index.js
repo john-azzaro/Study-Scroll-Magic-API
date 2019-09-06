9389a53990b43db03d0d185b884fea0fc6105553
John Azzaro <jazzaro@temple.edu>
2019-09-05 21:12:12 -0700

    added css, notes js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function splitScroll() {
-    const controller = new ScrollMagic.Controller
+    const controller = new ScrollMagic.Controller();                 // Controller that handles all the scrolls on the page.
 }
 
 
