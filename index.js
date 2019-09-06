a10eb498a2ef857d6fb741830b56fbbb6c7b2676
John Azzaro <jazzaro@temple.edu>
2019-09-06 10:18:35 -0700

    adjusted setPin trigger, triggerElement, notes

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function splitScroll() {                                               // When S
     const controller = new ScrollMagic.Controller();                   // Controller that handles all the scrolls on the page.
 
     new ScrollMagic.Scene({                                            // create a new Scrollmagic scene with an object with the following properties:
-        duration: '200%',                                              // scroll duration that lasts 500 pixels.
-        triggerElement: '.product-title',                                // Trigger element that defines what will trigger the scroll (in this case, it is "product-title") so when it reaches this element, scroll effect will execute
+        duration: '200%',                                              // scroll duration.
+        triggerElement: '.product-title',                              // Trigger element that defines what will trigger the scroll (in this case, it is "product-title") so when it reaches this element, scroll effect will execute
         triggerHook: 0                                                 // then to modify the trigger position (i.e. adjust the trigger point) (1 is bottom, 0 is top.)
     })
     // .addIndicators()                                                // method to see the triggers during development
-    .setPin('.product-title')                                            // setpin adds position:fixed to the title when the tigger passes the point. (note: add margin:0!important to product-title to avoid bug)
+    .setPin('.product-title')                                          // setpin adds position:fixed to the title when the tigger passes the point. (note: add margin:0!important to product-title to avoid bug)
     .addTo(controller);                                                // then hook up to the controller to initiate.
 }
 
