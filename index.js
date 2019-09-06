c35b00870464743d9828eb456f312c068475ebf2
John Azzaro <jazzaro@temple.edu>
2019-09-06 09:52:34 -0700

    adjusted setPin trigger, triggerElement

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function splitScroll() {                                               // When S
 
     new ScrollMagic.Scene({                                            // create a new Scrollmagic scene with an object with the following properties:
         duration: '200%',                                              // scroll duration that lasts 500 pixels.
-        triggerElement: '.about-title',                                // Trigger element that defines what will trigger the scroll (in this case, it is "about-title") so when it reaches this element, scroll effect will execute
+        triggerElement: '.product-title',                                // Trigger element that defines what will trigger the scroll (in this case, it is "product-title") so when it reaches this element, scroll effect will execute
         triggerHook: 0                                                 // then to modify the trigger position (i.e. adjust the trigger point) (1 is bottom, 0 is top.)
     })
     // .addIndicators()                                                // method to see the triggers during development
-    .setPin('.about-title')                                            // setpin adds position:fixed to the title when the tigger passes the point. (note: add margin:0!important to about-title to avoid bug)
+    .setPin('.product-title')                                            // setpin adds position:fixed to the title when the tigger passes the point. (note: add margin:0!important to product-title to avoid bug)
     .addTo(controller);                                                // then hook up to the controller to initiate.
 }
 
